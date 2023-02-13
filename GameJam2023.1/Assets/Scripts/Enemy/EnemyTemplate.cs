1845250e64bc397c719080cb89c276a8f84d48e0
Alvin Stilley <111103467+AStilley@users.noreply.github.com>
2023-02-13 01:02:40 -0600

    Enemy v2
    -Added expValue variable
    -Added enemy is destroyed when health is zero or less
    -Testing enemy movement

--- GameJam2023.1/Assets/Scripts/Enemy/EnemyTemplate.cs
+++ GameJam2023.1/Assets/Scripts/Enemy/EnemyTemplate.cs
@@ -5,25 +5,45 @@ using UnityEngine;
 public class EnemyTemplate : MonoBehaviour
 {
     //public GameObject enemyBullet;
+    private int currHealth;
+
     public Enemy enemy;
     private IEnumerator coroutine;
 
+    void Awake()
+    {
+
+
+        currHealth = enemy.enemyHealth;
+        //Stores the current health of the enemy. The enemy type holds the maximum health.
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Test");
+        //Debug.Log("Test");
         coroutine = Shoot(enemy.enemyFireRate);
         StartCoroutine(coroutine);
+        //The enemy shoots constantly every x seconds, where x is a float value assigned in the enemy type.
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (currHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+        //Things die when they are killed.
 
 
 
-       // Instantiate(enemy.bulletPrefab, transform.position, Quaternion.identity);
+
+
+        transform.position = transform.position + new Vector3(.05f * Mathf.Cos(5 * Time.time), -.0025f* enemy.enemySpeed, 0f);
+
+
+
     }
     IEnumerator Shoot(float waitTime)
     {
@@ -33,5 +53,6 @@ public class EnemyTemplate : MonoBehaviour
             Instantiate(enemy.bulletPrefab, transform.position, Quaternion.identity);
             yield return new WaitForSeconds(waitTime);
         }
+        //Shoots a bullet determined by the enemy type. Small, medium or large.
     }
 }
